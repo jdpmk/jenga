da03c6f388ce3ec51b722290954d34f77886b2fa
Joydeep Mukherjee <joydeepm02@gmail.com>
2022-12-22 21:09:21 -0500

    Add functionality to read program from a file

--- bin/main.ml
+++ bin/main.ml
@@ -1,24 +1,27 @@
+open Sys
 open Jenga.Exceptions
 open Jenga.Lexer
 open Jenga.Parser
 open Jenga.Types
 
+let print_usage args = Printf.fprintf stderr "usage: %s program.jg" args.(0)
+
 let print_error_with_loc message lexbuf =
   let pos = lexbuf.Lexing.lex_curr_p in
-  let filename = pos.pos_fname in
   let line = pos.pos_lnum in
   let column = pos.pos_cnum - pos.pos_bol + 1 in
-  Printf.printf "error: %s:%d:%d %s" filename line column message
+  Printf.fprintf stderr "error: %d:%d %s" line column message
 
 let () =
-  (* TODO: read program from file provided via command line args *)
-  let example_program = "1 2 + println" in
-  let program_code = example_program in
-  let lexbuf = Lexing.from_string program_code in
-  let program =
-    try parse_program read_token lexbuf
-    with SyntaxError message ->
-      print_error_with_loc message lexbuf;
-      exit 1
-  in
-  type_check program
+  if Array.length argv <> 2 then print_usage argv
+  else
+    let filepath = argv.(1) in
+    let ic = open_in filepath in
+    let lexbuf = Lexing.from_channel ~with_positions:true ic in
+    let program =
+      try parse_program read_token lexbuf
+      with SyntaxError message ->
+        print_error_with_loc message lexbuf;
+        exit 1
+    in
+    type_check program
